23884b980afaf6998180b791b56943263530f488
xMausoleuMx <j0hntw1ll1ams95@gmail.com>
2016-12-22 17:12:35 -0600

    Added the ability to decrypt a phrase as well as encrypt one

--- Source.cpp
+++ Source.cpp
@@ -5,33 +5,63 @@ using namespace std;
 
 int main()
 {
-	int letterShift;
+	int letterShift, counter = 0, choice;
 	string initialPhrase;
-	int counter = 0;
-	std::cout << "Enter the phrase you wish to encrypt\n";
+	std::cout << "Enter your phrase\n";
 	std::getline(std::cin, initialPhrase);
-	std::cout << "What is the shift amount desired?\n";
-	std::cin >> letterShift;
-	//making sure the shift stays within the alphabet
-	letterShift %= 26;
-	while (counter < initialPhrase.length())
+	std::cout << "Would you like to\n1.Encrypt\n2.Decrypt\n";
+	std::cin >> choice;
+	switch (choice)
 	{
-		//shifting the letters based on the given shift
-		if (initialPhrase[counter] <= 'z' && initialPhrase[counter] >= 'a')
-		{
-			if ((initialPhrase[counter] + letterShift) < 'z')
-				initialPhrase[counter] = (initialPhrase[counter] + letterShift);
-			else
-				initialPhrase[counter] = (initialPhrase[counter] + letterShift) - 'z' + 'a';
-		}
-		if (initialPhrase[counter] <= 'Z' && initialPhrase[counter] >= 'A')
-		{
-			if ((initialPhrase[counter] + letterShift)< 'Z')
-				initialPhrase[counter] = (initialPhrase[counter] + letterShift);
-			else
-				initialPhrase[counter] = (initialPhrase[counter] + letterShift) - 'Z' + 'A';
-		}
-		counter++;
+		case 1:
+			std::cout << "What is the shift amount desired?\n";
+			std::cin >> letterShift;
+			//making sure the shift stays within the alphabet
+			letterShift %= 26;
+			while (counter < initialPhrase.length())
+			{
+				//shifting the letters based on the given shift
+				if (initialPhrase[counter] <= 'z' && initialPhrase[counter] >= 'a')
+				{
+					if ((initialPhrase[counter] + letterShift) < 'z')
+						initialPhrase[counter] = (initialPhrase[counter] + letterShift);
+					else
+						initialPhrase[counter] = (initialPhrase[counter] + letterShift) - 'z' + 'a';
+				}
+				if (initialPhrase[counter] <= 'Z' && initialPhrase[counter] >= 'A')
+				{
+					if ((initialPhrase[counter] + letterShift) < 'Z')
+						initialPhrase[counter] = (initialPhrase[counter] + letterShift);
+					else
+						initialPhrase[counter] = (initialPhrase[counter] + letterShift) - 'Z' + 'A';
+				}
+				counter++;
+			}
+			std::cout << "\nHere is your encrypted text: " << initialPhrase << endl;
+			break;
+		case 2:
+			std::cout << "What is decryption key?\n";
+			std::cin >> letterShift;
+			letterShift %= 26;
+			while (counter < initialPhrase.length())
+			{
+				//shifting the letters based on the given shift
+				if (initialPhrase[counter] <= 'z' && initialPhrase[counter] >= 'a')
+				{
+					if ((initialPhrase[counter] - letterShift) > 'a')
+						initialPhrase[counter] = (initialPhrase[counter] - letterShift);
+					else
+						initialPhrase[counter] = (initialPhrase[counter] - letterShift) + 'z'-'a';
+				}
+				if (initialPhrase[counter] <= 'Z' && initialPhrase[counter] >= 'A')
+				{
+					if ((initialPhrase[counter] - letterShift) > 'A')
+						initialPhrase[counter] = (initialPhrase[counter] - letterShift);
+					else
+						initialPhrase[counter] = (initialPhrase[counter] - letterShift) + 'Z' - 'A';
+				}
+				counter++;
+			}
+			std::cout << "\nHere is your decrypted text: " << initialPhrase << endl;
 	}
-	std::cout << "\nHere is your encrypted text: " << initialPhrase << endl;
 }
